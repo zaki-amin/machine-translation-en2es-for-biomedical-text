a287f9e841e85b086c9b45a10e6c0baa46d3538f
zaki-amin <zakiamin0402@gmail.com>
2024-05-05 10:46:52 +0200

    Add translation and output file writing to translation_pipeline.py

--- hpo_translator/translation_pipeline.py
+++ hpo_translator/translation_pipeline.py
@@ -2,21 +2,40 @@ import pandas as pd
 
 from dictionaries.abbreviations import Abbreviations
 from evaluation.official.string_similarity import SimilarityMetric
+from hpo_translator.src.models import MarianMTConfig, NMTModelConfig
+from hpo_translator.src.translate import translate
 
 
-def translate_input(input_file: str):
+def translate_input(input_file: str,
+                    output_file: str,
+                    checkpoint: str | None = None,
+                    config: NMTModelConfig = MarianMTConfig()):
     """Translates the input .txt file from English to Spanish.
     Applies pre-processing, the model and post-processing.
     Writes the output CSV to a file with the following columns:
     - 'original': the original English sentence
     - 'translation': the actual provided translation
 
-    :param input_file: the input file to translate, a text file with each sentence on a new line"""
+    :param input_file: the input file to translate, a .txt file with each sentence on a new line
+    :param output_file: the output file to write results to
+    :param checkpoint: the name of the model checkpoint to use. If not given, defaults to clinical MarianMT
+    :param config: the configuration of the model to use. If not given, defaults to MarianMTConfig"""
     with open(input_file, 'r') as file:
         english_inputs = [line.strip() for line in file.readlines()]
-    english_inputs = preprocess(english_inputs)
-    # do some model stuff here
-    pass
+
+    # Preprocessing
+    abbreviations = Abbreviations(
+        "/Users/zaki/PycharmProjects/hpo_translation/dictionaries/processed/abbreviations.jsonl")
+    english_inputs = abbreviations.preprocess(english_inputs)
+
+    # Translation with model
+    translated = translate(english_inputs, checkpoint, config)
+
+    # Postprocessing
+    synonyms_filename = "/Users/zaki/PycharmProjects/hpo_translation/dictionaries/processed/synonyms.jsonl"
+    # translated = postprocess(translated, synonyms_filename)
+
+    write_output(english_inputs, translated, output_file)
 
 
 def preprocess(english_inputs: list[str], abbreviations_filename: str) -> list[str]:
@@ -26,6 +45,13 @@ def preprocess(english_inputs: list[str], abbreviations_filename: str) -> list[s
 
 def postprocess(df: pd.DataFrame, synonyms_filename: str) -> list[str]:
     pass
+
+
+def write_output(sources: list[str], targets: list[str], output_file: str):
+    """Writes the English sources and Spanish targets to a CSV file."""
+    data = {'en': sources, 'es': targets}
+    df = pd.DataFrame(data)
+    df.to_csv(output_file, index=False, header=True)
 
 
 def evaluate_translations(df: pd.DataFrame) -> pd.DataFrame:
@@ -62,3 +88,11 @@ def translate_and_evaluate(input_file: str, output_file: str):
 
     :param input_file: the input file to translate and evaluate"""
     pass
+
+
+def main():
+    translate_input("input.txt", "output.csv")
+
+
+if __name__ == "__main__":
+    main()
