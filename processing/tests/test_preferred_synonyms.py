8fc85989eb30d93b38291c96f43dd2cffc311248
zaki-amin <zakiamin0402@gmail.com>
2024-05-09 17:53:43 +0200

    Create training dataset from preferred_en2es

--- processing/tests/test_preferred_synonyms.py
+++ processing/tests/test_preferred_synonyms.py
@@ -21,7 +21,7 @@ class TestPreferredSynonyms(unittest.TestCase):
                       "preferred synonym dictionary should include feminine 'ácido-básica'")
 
     def test_postprocess_translation(self):
-        phrase = 'El paciente parece sufrir problemas con el válvula del corazón'
+        phrase = 'El paciente parece sufrir problemas con la válvula del corazón'
         self.assertEqual(self.preferred_synonyms.postprocess_translation(phrase),
                          'El paciente parece sufrir problemas con la válvula cardíaca',
                          "synonym replacement should change 'válvula del corazón' to 'válvula cardíaca'")
