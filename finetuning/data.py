7f664560d228e4ed6db89ca8762395c733686a6a
zaki-amin <zakiamin0402@gmail.com>
2024-05-14 11:29:00 +0200

    Add MarianTokenizer

--- finetuning/data.py
+++ finetuning/data.py
@@ -1,8 +1,9 @@
 from datasets import load_dataset, DatasetDict
+from transformers import MarianTokenizer
 
 
 def load_corpus(data_filename: str, validation_proportion: float, seed: int) -> DatasetDict:
-    """Reads a JSONL file for a parallel corpus
+    """Reads a JSONL file for a parallel corpus into a DatasetDict for fine-tuning
     :param data_filename: path to the JSONL file containing the parallel corpus
     :param validation_proportion: the proportion of the data to use for validation
     :param seed: the random seed to use for splitting the data"""
@@ -11,3 +12,9 @@ def load_corpus(data_filename: str, validation_proportion: float, seed: int) -> 
     split_datasets = training_data["train"].train_test_split(train_size=train_proportion, seed=seed)
     split_datasets["validation"] = split_datasets.pop("test")
     return split_datasets
+
+
+def marian_tokenizer() -> MarianTokenizer:
+    """Returns the original MarianTokenizer for Helsinki-NLP/opus-mt-en-es"""
+    checkpoint_name = "Helsinki-NLP/opus-mt-en-es"
+    return MarianTokenizer.from_pretrained(checkpoint_name)
--- finetuning/test_finetuning.py
+++ finetuning/test_finetuning.py
@@ -1,17 +1,26 @@
 import unittest
 
-from finetuning.data import load_corpus
+from finetuning.data import load_corpus, marian_tokenizer
 
 
 class TestFineTuning(unittest.TestCase):
-    def test_data_loads_correctly(self):
-        filepath = '../corpus/train/medline.jsonl'
-        data = load_corpus(filepath, 0.2, 42)
+    filepath = '../corpus/train/medline.jsonl'
+    data = load_corpus(filepath, 0.2, 42)
+    tokenizer = marian_tokenizer()
 
-        first_train_example = data['train'][0]
+    def test_data_loads_correctly(self):
+        first_train_example = self.data['train'][0]
         self.assertEqual(first_train_example['en'], 'Shin splints.')
         self.assertEqual(first_train_example['es'], 'Dolor en las espinillas.')
 
-        first_validation_example = data['validation'][0]
+        first_validation_example = self.data['validation'][0]
         self.assertEqual(first_validation_example['en'], 'Micrognathia.')
         self.assertEqual(first_validation_example['es'], 'Micrognacia.')
+
+    def test_tokenizer_transforms_text(self):
+        text = ("Underterminate colitis designates a rare inflammatory bowel disease that clinically resembles Crohn's "
+                "disease and ulcerative colitis (see these terms) but that cannot be diagnosed as one of them after "
+                "examination of an intestinal resection specimen.")
+        tokens = self.tokenizer(text)
+        self.assertIn("input_ids", tokens, "Tokenization failed to produce input_ids")
+        print(tokens)
