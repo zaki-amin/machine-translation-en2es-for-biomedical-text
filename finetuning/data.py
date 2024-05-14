cf159f046e7d5e6d62d23b1eef77468ea875b4aa
zaki-amin <zakiamin0402@gmail.com>
2024-05-14 10:29:33 +0200

    Validation split in data loading

--- finetuning/data.py
+++ finetuning/data.py
@@ -1,8 +1,13 @@
 from datasets import load_dataset, DatasetDict
 
 
-def load_corpus(data_filename: str) -> DatasetDict:
+def load_corpus(data_filename: str, validation_proportion: float, seed: int) -> DatasetDict:
     """Reads a JSONL file for a parallel corpus
-    :param data_filename: path to the JSONL file containing the parallel corpus"""
+    :param data_filename: path to the JSONL file containing the parallel corpus
+    :param validation_proportion: the proportion of the data to use for validation
+    :param seed: the random seed to use for splitting the data"""
     training_data = load_dataset("json", data_files=data_filename)
-    return training_data["train"]
+    train_proportion = 1 - validation_proportion
+    split_datasets = training_data["train"].train_test_split(train_size=train_proportion, seed=seed)
+    split_datasets["validation"] = split_datasets.pop("test")
+    return split_datasets
--- finetuning/test_finetuning.py
+++ finetuning/test_finetuning.py
@@ -6,9 +6,12 @@ from finetuning.data import load_corpus
 class TestFineTuning(unittest.TestCase):
     def test_data_loads_correctly(self):
         filepath = '../corpus/train/medline.jsonl'
-        data = load_corpus(filepath)
-        first_line = data[0]
-        self.assertEqual(first_line['en'],
-                         'To check for nystagmus, the provider may use the following procedure.')
-        self.assertEqual(first_line['es'],
-                         'Para examinar el nistagmo, el proveedor puede usar el siguiente procedimiento.')
+        data = load_corpus(filepath, 0.2, 42)
+
+        first_train_example = data['train'][0]
+        self.assertEqual(first_train_example['en'], 'Shin splints.')
+        self.assertEqual(first_train_example['es'], 'Dolor en las espinillas.')
+
+        first_validation_example = data['validation'][0]
+        self.assertEqual(first_validation_example['en'], 'Micrognathia.')
+        self.assertEqual(first_validation_example['es'], 'Micrognacia.')
