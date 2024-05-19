a17e70d36a350dee1fa0f505151d075b3eb5604a
zaki-amin <zakiamin0402@gmail.com>
2024-05-19 10:04:05 +0200

    Switch khresmoi train and test files and divide clinspen into train and test 85:15

---
+++ data_handling/shuffle_and_split.py
@@ -0,0 +1,36 @@
+import json
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def shuffle_and_split(input_jsonl: str,
+                      test_proportion: float,
+                      seed: int,
+                      output_name: str):
+    """Reads in data, shuffles it and splits into two files"""
+    english, spanish = [], []
+    with open(input_jsonl, 'r') as file:
+        for line in file:
+            data = json.loads(line)
+            en, es = data["en"], data["es"]
+            english.append(en)
+            spanish.append(es)
+
+    english = np.array(english)
+    spanish = np.array(spanish)
+    en_train, en_test, es_train, es_test = train_test_split(english,
+                                                            spanish,
+                                                            test_size=test_proportion,
+                                                            random_state=seed)
+    with open(f"{output_name}_train.jsonl", 'w') as train:
+        for en, es in zip(en_train, es_train):
+            train.write(json.dumps({"en": en, "es": es}) + "\n")
+    with open(f"{output_name}_test.jsonl", 'w') as test:
+        for en, es in zip(en_test, es_test):
+            test.write(json.dumps({"en": en, "es": es}) + "\n")
+
+
+if __name__ == '__main__':
+    input_filename = "/Users/zaki/PycharmProjects/hpo_translation/corpus/test/clinspen.jsonl"
+    shuffle_and_split(input_filename, 0.15, 64, "clinspen")
